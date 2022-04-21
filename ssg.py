c2db3e44077438d5ce9a0722cb0a412091c4e9bb
Sophie Bradshaw <sophie.bradshaw97@gmail.com>
2022-04-21 21:17:56 +0100

    test

--- ssg.py
+++ ssg.py
@@ -1,8 +1,11 @@
 import typer
+
 from ssg.site import Site
 
+
 def main(source="content", dest="dist"):
-    config={config: "Source", dest: "dest"}
+    config= {"Source": source, "dest": dest}
+
     Site(**config).build()
 
 typer.run(main)    
