3a5ac642110b304a9252b374633b861b6d50cc0a
Sophie Bradshaw <sophie.bradshaw97@gmail.com>
2022-05-05 21:35:05 +0100

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -10,6 +10,18 @@ class Site:
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
 
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
+
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print("not implemented")                    
+
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
